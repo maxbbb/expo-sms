f0466e64610124a45e11bccda9201ce37f070498
Kane Thomas <kmt8vn@virginia.edu>
2020-03-26 15:48:01 -0400

    bare expo sms

--- src/SMS.ts
+++ src/SMS.ts
@@ -5,29 +5,6 @@ type SMSResponse = {
   result: 'unknown' | 'sent' | 'cancelled';
 };
 
-type MSMessageLayout = {
-  mediaFileUrl: string;
-  caption?: string;
-  imageTitle?: string;
-  imageSubtitle?: string;
-  subcaption?: string;
-  trailingCaption?: string;
-  trailingSubcaption?: string;
-}
-
-type URLQueryItems = any;
-
-// example
-// {
-//   gameUrl: string;
-//   gameId: string;
-// }
-
-type MSMessageInfo = {
-  urlQueryItems: any;
-  layoutParams: MSMessageLayout;
-}
-
 export async function sendSMSAsync(
   addresses: string | string[],
   message: string
@@ -37,18 +14,6 @@ export async function sendSMSAsync(
     throw new Error(`SMS.sendSMSAsync is not supported on ${Platform.OS}`);
   }
   return ExpoSMS.sendSMSAsync(finalAddresses, message);
-}
-
-export async function sendSMSWithiMessageAsync(
-  addresses: string | string[],
-  message: string,
-  imessageInfo: MSMessageInfo
-): Promise<SMSResponse> {
-  const finalAddresses = Array.isArray(addresses) ? addresses : [addresses];
-  if (!ExpoSMS.sendSMSAsync) {
-    throw new Error(`SMS.sendSMSAsync is not supported on ${Platform.OS}`);
-  }
-  return ExpoSMS.sendSMSWithiMessageAsync(finalAddresses, message, imessageInfo);
 }
 
 export async function isAvailableAsync(): Promise<boolean> {
